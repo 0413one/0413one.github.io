a4ff569c8fbb53f5c6aec0bdae99b3c76ef1828e
Karina <onejialin@outlook.com>
2025-12-24 23:59:05 +0100

    优化判断逻辑

--- link-break-iframe.js
+++ link-break-iframe.js
@@ -1,11 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // 判断页面是否有目标特征类名
-  const isTargetPage = document.body.classList.contains('link-break-iframe');
-
-  // 非目标页面不执行任何操作
-  if (!isTargetPage) return;
-
-  // 所有链接 新标签页打开及安全属性
   const allLinks = document.querySelectorAll('a[href]');
   allLinks.forEach(link => {
     link.target = '_blank';
