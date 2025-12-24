5c2b88445b11f9dea4da9ae87a13bbbf485f516a
Karina <onejialin@outlook.com>
2025-12-25 00:11:43 +0100

    更新 link-break-iframe.js

--- link-break-iframe.js
+++ link-break-iframe.js
@@ -1,6 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const allLinks = document.querySelectorAll('a[href]');
-  allLinks.forEach(link => {
-    link.target = '_blank';
-    link.rel = 'noopener noreferrer';
-  });
+const allLinks = document.querySelectorAll('a[href]');
+allLinks.forEach(function(link) {
+  link.target = '_blank';
+  link.rel = 'noopener noreferrer';
+});
