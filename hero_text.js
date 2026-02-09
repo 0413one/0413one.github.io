df216ea841296d6d8825265c1b527c18dd0515eb
Karina <onejialin@outlook.com>
2026-02-09 03:24:21 +0100

    Create hero_text.js

---
+++ hero_text.js
@@ -0,0 +1,17 @@
+const el = document.getElementById("hero-line");
+el.style.transition = "opacity 3s"; // 淡入淡出时间
+
+function pickRandomSentence() {
+  const index = Math.floor(Math.random() * HERO_SENTENCES.length);
+  return HERO_SENTENCES[index];
+}
+
+function updateSentence() {
+  el.style.opacity = 0; // 先淡出
+  setTimeout(() => {
+    el.textContent = "🍥" + pickRandomSentence(); // 更新文字
+    el.style.opacity = 1; // 再淡入
+  }, 1000); // 等待淡出时间结束
+}
+
+setInterval(updateSentence, 5000);
